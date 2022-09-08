30a370aa957f312bace8dee835f438a70d0ee135
aleksey <akusakin@proton.me>
2022-09-08 18:18:22 +0300

    adds moving evelator

--- src/components/AppShaft.vue
+++ src/components/AppShaft.vue
@@ -1,16 +1,72 @@
 <template>
   <div
     class="shaft"
-    :style="{ height: floorsCount * 100 + 'px' }"
+    :style="{
+      height: floorsCount * 100 + 'px',
+    }"
   >
-    <div class="shaft__elevator"></div>
+    <div
+      class="shaft__elevator"
+      :style="{
+        transform: `translateY(${position}px`,
+      }"
+    >
+      {{ currentFloorIndex }} этаж
+    </div>
   </div>
 </template>
 
 <script lang="ts">
+const speed = 1000 / 100; // 1 этаж(100px) за 1000ms
+const enum EvelatorState {
+  Free,
+  Moving,
+  Pending,
+}
+
 export default {
   props: {
     floorsCount: Number,
+  },
+  data() {
+    const currentFloorIndex = 3;
+    return {
+      state: EvelatorState.Free,
+      currentFloorIndex: currentFloorIndex,
+      position: this.calcalatePosition(currentFloorIndex),
+    };
+  },
+  created() {
+    this.moveShaft(5);
+  },
+  methods: {
+    calcalatePosition(floorIndex: number): number {
+      return -(floorIndex - 1) * 100;
+    },
+    moveShaft(nextFloorIndex: number) {
+      if (nextFloorIndex === this.currentFloorIndex) {
+        return;
+      }
+
+      const nextPos = this.calcalatePosition(nextFloorIndex);
+      const isUpDirection = nextFloorIndex > this.currentFloorIndex;
+
+      this.currentFloorIndex = nextFloorIndex;
+      this.state = EvelatorState.Moving;
+
+      const interval = setInterval(() => {
+        if (nextPos === this.position) {
+          this.stopMoving(interval);
+          return;
+        }
+
+        this.position = isUpDirection ? this.position - 1 : this.position + 1;
+      }, speed);
+    },
+    stopMoving(interval: number) {
+      clearInterval(interval);
+      this.state = EvelatorState.Pending;
+    },
   },
 };
 </script>
